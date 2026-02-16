ddb8279c9eb694cd1e7493d1acf163fb5bde1a80
hazzem-web <hazzem.eljoker@gmail.com>
2026-02-16 06:40:34 +0200

    install packages and connect to database

---
+++ src/database/connection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { mongoURI } from "../../config/env.service.js";
+
+export const databaseConnection = async ()=> { 
+    await mongoose.connect(mongoURI).then(()=>{
+        console.log('database connected successfully');
+    }).catch((err)=>{
+        console.error('cant connect to database: ', err)
+    })
+}
