a686ea6a2ba52e6bd19b2ec9d04c57701ffce9ac
hazzem-web <hazzem.eljoker@gmail.com>
2026-03-29 11:16:00 +0200

    feat(app): configure static file serving, body parsing middleware and install helmet for upcoming security hardening

--- src/app.controller.js
+++ src/app.controller.js
@@ -2,19 +2,28 @@ import express from 'express';
 import { databaseConnection, redisConnect } from './database/index.js';
 import { BASE_URL, Port } from '../config/index.js';
 import { globalErrorHandler } from './common/utils/responses/index.js';
+import path from 'path';
 import cors from 'cors';
 import authRouter from './modules/auth/auth.controller.js';
 import userRouter from './modules/users/user.controller.js';
 import messageRouter from './modules/messages/message.controller.js';
 import './cron.js';
-const allowedOrigins = [BASE_URL    ];
+import helmet from 'helmet';
+const allowedOrigins = [BASE_URL];
 export const bootstrap = async ()=>{
     const app = express();
+    const uploadsPath = path.join(process.cwd(), 'uploads');
+    console.log("serving static files from: ", uploadsPath);
+    app.use(express.static(uploadsPath));
+    app.use(express.static('public'));
     app.use(express.json());
+    app.use(express.urlencoded({extended: false}))
+    app.use(helmet());
     app.use(cors({
         origin: [...allowedOrigins],
         credentials: true
     }));
+
     app.use('/uploads',express.static('uploads'))
     app.use('/auth', authRouter);
     app.use('/users',userRouter);   
