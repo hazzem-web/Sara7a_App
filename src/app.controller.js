746a1c46c1227edc38a3fe3bfebbd7306be05e4c
hazzem-web <hazzem.eljoker@gmail.com>
2026-03-29 10:13:59 +0200

    refactor(app): simplify cors configuration

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import { databaseConnection, redisConnect } from './database/index.js';
-import { Port } from '../config/index.js';
+import { BASE_URL, Port } from '../config/index.js';
 import { globalErrorHandler } from './common/utils/responses/index.js';
 import cors from 'cors';
 import authRouter from './modules/auth/auth.controller.js';
 import userRouter from './modules/users/user.controller.js';
 import messageRouter from './modules/messages/message.controller.js';
 import './cron.js';
-
+const allowedOrigins = [BASE_URL    ];
 export const bootstrap = async ()=>{
     const app = express();
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+        origin: [...allowedOrigins],
+        credentials: true
+    }));
     app.use('/uploads',express.static('uploads'))
     app.use('/auth', authRouter);
     app.use('/users',userRouter);   
